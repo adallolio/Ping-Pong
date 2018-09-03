73cf92befb51a90329dc3d07048473ca4c5ef0e2
Alberto Dallolio <albydallo@gmail.com>
2018-09-03 16:16:17 +0200

    change struct, added main

---
+++ tests/uart/main.c
@@ -0,0 +1,29 @@
+// Main
+
+#ifndef F_CPU
+#define F_CPU 4915200UL
+#endif
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include "uart_test.h"
+
+void main(void){
+
+	unsigned long cpu_speed = F_CPU;
+    UART_Init(cpu_speed);
+
+	while(1){
+		//message = UART_receive();
+		//printf("%c\n", message);
+		
+		UART_send('a');
+		//printf("Is it transmitted?!?!?!?!?!");
+		
+		_delay_ms(100);
+
+		//stdout = &uart_io;
+		//stdin = &uart_io;
+	}
+}
--- tests/uart/uart_test.h
+++ tests/uart/uart_test.h
@@ -1,18 +1,17 @@
 // TEST UART COMM
 // Alberto Dallolio
 
-// Check if correct.
+#ifndef UART_TEST_H
+#define UART_TEST_H
 
-#ifndef UART_H
-#define UART_H
+void UART_Init(unsigned long clock);
+int UART_send(unsigned char data);
+int UART_receive(void);
 
-#define F_CPU 4915200 // Informs the compiler: how many steps in one second.
-#define FOSC 4915200  // 49152MHz
-#define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1  //  (F_CPU / (16 * BAUD)) - 1 = (4915200/(16*9600)) - 1
 
-void USART_Init(unsigned int ubrr);
-void USART_trans (unsigned char data);
-unsigned char USART_rec (void);
+//#define F_CPU 4915200UL // Informs the compiler: how many steps in one second.
+//#define FOSC 4915200  // 49152MHz
+//#define BAUD 9600
+//#define MYUBRR (F_CPU / (16 * BAUD)) - 1 //FOSC/16/BAUD-1  //  (F_CPU / (16 * BAUD)) - 1 = (4915200/(16*9600)) - 1
 
 #endif
