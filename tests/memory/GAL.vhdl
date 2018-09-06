acbe815950139c3c70e89511fa663bb3abf2c4ae
Alberto Dallolio <albydallo@gmail.com>
2018-09-06 15:49:14 +0200

    Update GAL.vhdl

--- tests/memory/GAL.vhdl
+++ tests/memory/GAL.vhdl
@@ -30,6 +30,10 @@ end address_decoder;
 
 architecture behave of address_decoder is
 
--- IMPLEMENT STUFF HERE
+    ram_cs <= a11;
+	adc_cs <= NOT (NOT a11 AND a10);
+	oled_cs <= NOT (NOT a11 AND NOT a10);
+	oled_dc <= NOT a11 AND NOT a10 AND a9;
+    -- or oled_dc <= a9; ?
 
 end behave;
